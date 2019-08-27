55116002e8fbff820aae9200f48b053b1ecf7700
wangjun12677 <14185638@qq.com>
2019-08-27 14:28:29 +0800

    init

--- dao/dbUtil.js
+++ dao/dbUtil.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 function createConnection(){
     var connection = mysql.createConnection({
         host:"192.168.1.2",
-        port:"3306",
+        port:"80",
         user:"root",
         password:"wangjun12677",
         database:"my_blog"
